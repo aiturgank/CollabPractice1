7887feba672313e7c78257361254d847f0868da7
Nadira777 <hralakz@gmail.com>
2024-09-20 12:37:39 -0400

    changes

--- src/utils/javaTask.java
+++ src/utils/javaTask.java
@@ -1,10 +1,18 @@
 package utils;
 
+import java.util.ArrayList;
+
 public class javaTask {
 
     public static void main(String[] args) {
 
         System.out.println((12345));
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("one");
+        list.add("two");
+
+        System.out.println(list);
 
     }
 }
